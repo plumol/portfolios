8e84eb500362783a114bafc6e6c0bf84794d6c11
plumol <kyle_lam@alumni.brown.edu>
2025-08-21 23:29:37 -0400

    Speed insights vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Script from 'next/script'
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -36,6 +37,7 @@ export default function RootLayout({
         <CustomCursor />
         {children}
         <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
